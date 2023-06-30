3b0b74a613ad527523c2bb74768643e5e455c81a
Alex <pixel102rus@gmail.com>
2023-07-01 00:31:07 +0300

    add service object cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,64 +1,32 @@
 class CartsController < ApplicationController
-  before_action :set_orderable, only: %i[ remove ]
+  before_action :set_cart
+  before_action :set_orderable, only: [:remove]
+
   def show
+    @cart = current_user.cart
     @render_cart = true
+    @cart_data = Carts::CartService.call(@cart)
   end
 
   def add
     @product = Product.find_by(id: params[:id])
     quantity = params[:quantity].to_i
-    current_orderable = @cart.orderables.find_by(product_id: @product.id)
-    if current_orderable && quantity > 0
-      current_orderable.update(quantity:)
-    elsif quantity <= 0
-      current_orderable.destroy
-    else
-      @cart.orderables.create(product: @product, quantity:)
-      redirect_to carts_path
-    end
+    Carts::CartService.call(@cart, @product, quantity)
+    redirect_to cart_path
   end
+
   def remove
-    Orderable.find_by(id: params[:id]).destroy
-    redirect_to carts_path
+    @orderable.destroy
+    redirect_to cart_path
   end
 
-end
+  private
 
-    private
-    # Use callbacks to share common setup or constraints between actions.
+  def set_cart
+    @cart = Cart.find_or_create_by(user_id: current_user.id)
+  end
+
   def set_orderable
-    @orderable = Orderable.find(params[:id])
+    @orderable = @cart.orderables.find(params[:id])
   end
-
-# class CartsController < ApplicationController
-#   before_action :set_orderable, only: %i[remove]
-#
-#   def show
-#     @render_cart = false
-#   end
-#
-#   def add
-#     @product = Product.find_by(id: params[:id])
-#     quantity = params[:quantity].to_i
-#     current_orderable = @cart.orderables.find_by(product_id: @product.id)
-#     if current_orderable && quantity.positive?
-#       current_orderable.update(quantity:)
-#     elsif quantity <= 0
-#       current_orderable.destroy
-#     else
-#       @cart.orderables.create(product: @product, quantity:)
-#       redirect_to carts_path
-#     end
-#   end
-#
-#   def remove
-#     @orderable.destroy
-#     redirect_to carts_path
-#   end
-#
-#   private
-#
-#   def set_orderable
-#     @orderable = Orderable.find(params[:id])
-#   end
-# end
+end
